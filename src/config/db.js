7d2101be865471da97bad68d761c68d783406651
PutraDipanala <putradipanala110494@gmail.com>
2026-09-02 18:03:58 +0800

    fix correct mysql root password

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'mysql.railway.internal',     // Sesuai nilai MYSQLHOST di layar
   port: 3306,                         // Port default MySQL Railway internal
   user: 'root',                       // Sesuai MYSQLUSER
-  password: 'WByQSZhcNRmwESmrcWaVVQEQXrPWPXJn', // Sesuai MYSQL_ROOT_PASSWORD di layar
+  password: 'WByQSZhcNRmwESmrcWaVVQeQXrPWPXJn', // Sesuai MYSQL_ROOT_PASSWORD di layar
   database: 'railway',                // Sesuai MYSQL_DATABASE
   waitForConnections: true,
   connectionLimit: 10,
